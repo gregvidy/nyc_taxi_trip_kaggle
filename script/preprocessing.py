6f991db2a3acce3773502d2770bda700c5020f1c
Gregorius <gregvidy@gmail.com>
2020-05-04 15:48:49 +0700

    modify preprocessing script

--- script/preprocessing.py
+++ script/preprocessing.py
@@ -1,8 +1,55 @@
 import pandas as pd
 import numpy as np
-import pickle
+import haversine
+import multiprocess
 
-def main():
+N_THREADS = 8
+
+def haversine_distance(x):
+    a_lat, a_lon, b_lat, b_lon = x_test
+    return haversine.haversine((a_lat, a_lon), (b_lat, b_lon))
+
+def apply_multithreaded(data, func):
+    pool = multiprocess.Pool(N_THREADS)
+    data = data.values
+    result = pool.map(func, data)
+    pool.close()
+    return result
+
+def preprocess_data(df):
+    df["store_and_fwd_flag"] = df["store_and_fwd_flag"].apply(lambda x: 1 if x == "Y" else 0)
+    df["pickup_datetime"] = pd.to_datetime(df["pickup_datetime"])
+    df["dropoff_datetime"] = pd.to_datetime(df["dropoff_datetime"])
+    df["day_of_week"] = [t.dayofweek for t in df["pickup_datetime"]]
+
+    # create distance features
+    df["dist_l1"] = np.abs(df["pickup_latitude"] - df["dropoff_latitude"]) + np.abs(df["pickup_longitude"] - df["dropoff_longitude"])
+    df["dist_l2"] = np.sqrt((df["pickup_latitude"] - df["dropoff_latitude"]) ** 2 + (df["pickup_longitude"] - df['dropoff_longitude']) ** 2)
+    df["dist_haversine"] = apply_multithreaded(df[['pickup_latitude', 'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude']], haversine_distance)
+
+    # create direction of travel features
+    df["delta_lat"] = df["dropoff_latitude"] - df["pickup_latitude"]
+    df["delta_long"] = df["dropoff_longitude"] - df["pickup_longitude"]
+    df["angle"] = (180 / np.pi) * np.arctan2(df["delta_lat"], df["delta_long"]) + 180
+
+    # creating traffic features
+    df['day'] = df['pickup_datetime'].dt.date
+    df['hour'] = df['pickup_datetime'].dt.hour
+    daily_traffic = df.groupby('day')['day'].count()  # Count the number of trips on each day
+    hourly_traffic = df.groupby('hour')['hour'].count()  # Count the number of trips in each hour
+    df['daily_count'] = df['day'].apply(lambda day: daily_traffic[day])
+    df['hourly_count'] = df['hour'].apply(lambda hour: hourly_traffic[hour])
+
+    # creating time estimate features
+    df['haversine_speed_km/s'] = df['dist_haversine'] / (df['trip_duration'] / 3600)  # Calculate haversine speed for training set
+    hourly_speed = df.groupby('hour')['haversine_speed'].mean()  # Find average haversine_speed for each hour in the training set
+    hourly_speed_fill = df['haversine_speed'].mean()  # Get mean across whole dataset for filling unknowns
+    df_hourly_speed = df['hour'].apply(lambda hour: hourly_speed[hour] if hour in hourly_speed else hourly_speed_fill)
+    df['haversine_speed_estim_km/h'] = df['dist_haversine'] / df_hourly_speed
+
+    return df
+
+if __name__ == "__main__":    
     print('Reading data into memory ...')
 
     # Read in the input dataset
@@ -10,43 +57,4 @@ def main():
     df_test = pd.read_csv('../data/test.csv')
 
     print('Read {} training data and {} testing data, preprocessing ...'.format(df_train.shape, df_test.shape))
-
-    # Convert store_and_fwd_flag into a binary numeric feature
-    df_train.loc[:, 'store_and_fwd_flag'] = df_train['store_and_fwd_flag'].apply(lambda x: int(x == 'Y'))
-    df_test.loc[:, 'store_and_fwd_flag'] = df_test['store_and_fwd_flag'].apply(lambda x: int(x == 'Y'))
-
-    # Parse pickup_datetime feature
-    df_train['timestamp'] = pd.to_datetime(df_train['pickup_datetime'])
-    df_test['timestamp'] = pd.to_datetime(df_test['pickup_datetime'])
-
-    # Create unix epoch time feature
-    # It is usually in nanoseconds, so we convert to seconds for readability
-    df_train['unix_time'] = [np.int64(t.value) / 1000000000 for t in df_train['timestamp']]
-    df_test['unix_time'] = [np.int64(t.value) / 1000000000 for t in df_test['timestamp']]
-
-    # Create minute of day feature
-    df_train['daily_minute'] = [t.hour * 60 + t.minute for t in df_train['timestamp']]
-    df_test['daily_minute'] = [t.hour * 60 + t.minute for t in df_test['timestamp']]
-
-    # Create day of week feature
-    df_train['day_of_week'] = [t.dayofweek for t in df_train['timestamp']]
-    df_test['day_of_week'] = [t.dayofweek for t in df_test['timestamp']]
-
-    # Extract target variable and keep it in memory
-    # Also do the same with test ID, because they are required for the Kaggle submission
-    y_train = df_train['trip_duration'].values
-    id_test = df_test['id'].values
-
-    # Drop non-training features from the set, as well as IDs and y values
-    x_train = df_train.drop(['timestamp', 'pickup_datetime', 'dropoff_datetime', 'id', 'trip_duration'], axis=1)
-    x_test = df_test.drop(['timestamp', 'pickup_datetime', 'id'], axis=1)
-
-    print(df_train.head())
-    print('Preprocessing complete, saving data to disk ...')
-
-    # Use pickle to dump our working data to disk so we can use it again in the future.
-    pickle.dump([df_train, df_test, x_train, x_test, y_train, id_test], open('preprocessed_data.bin', 'wb'), protocol=2)
-
-
-if __name__ == '__main__':
-    main()
+    
